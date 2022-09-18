b85d47a2697f47399f7f54544a6d05fca123f507
1305Burov <1305burov@gmail.com>
2022-09-18 15:14:14 +0300

    +

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,20 @@ import * as flsFunc from './moduls/func.js';
 flsFunc.isWebp();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+const checkbox = document.getElementById('checkbox');
+const more = document.getElementById('more');
+const moreButton = document.getElementById('moreButton');
+let isMore = false;
+checkbox.addEventListener('change', () => {
+    checkbox.parentElement.nextElementSibling.classList.toggle('nav_active');
+})
+
+moreButton.addEventListener('click', () => {
+    isMore = !isMore;
+    more.classList.toggle('projects-list__hidden');
+    isMore ? moreButton.textContent = 'View less' : moreButton.textContent = 'View more';
+})
+
+
